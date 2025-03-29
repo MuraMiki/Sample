026a82aa92df93b6ace621b1c882ce3ff1a71ddd
MuraMiki <imobatake29@gmail.com>
2025-03-29 11:59:02 +0800

    Javaプログラミング入門学習終了

---
+++ src/NotArithmeticExceptionSample.java
@@ -0,0 +1,13 @@
+
+public class NotArithmeticExceptionSample {
+
+    public static void main(String[] args) {
+        // double型10.0をdouble型0.0で除算
+        double a = 10.0 / 0.0; //この場合は例外が発生しない
+
+        System.out.println(a);
+
+
+    }
+
+}
